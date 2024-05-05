3189e3c4da1a53561a0a3390d9cfb767bcf28a31
rohan-kiratsata <kiratsatarohan1234@gmail.com>
2024-05-05 13:25:35 +0530

    color changed to green

--- components/ui-components/floating-button.tsx
+++ components/ui-components/floating-button.tsx
@@ -23,7 +23,7 @@ const FloatingButton = () => {
       initial={{ scale: 0 }} // Initial scale of 0
       animate={{ scale: 1 }} // Animate to scale of 1
       transition={{ duration: 0.5, type: "spring", stiffness: 200 }}
-      className="fixed bottom-4 right-4 z-50 rounded-full bg-primary-orange px-3 py-3 font-bold text-white shadow-lg hover:bg-orange-700"
+      className="fixed bottom-4 right-4 z-50 rounded-full bg-green-500 px-3 py-3 font-bold text-white shadow-lg hover:bg-green-700"
       onClick={() => openUrl(WP_LINK)}
     >
       <FaWhatsapp size={28} />
