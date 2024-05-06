5af6cbb03b62564fba182f3ddf4545603f1b2ec7
anilhoney59 <anilsuthar4@gmail.com>
2024-05-06 10:54:16 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Nav from "../components/nav";
 import FloatingButton from "../components/ui-components/floating-button";
 import "../styles/globals.css";
 import { inter } from "./fonts";
-import { Providers } from "./providers";
+
 
 import type { Metadata } from "next";
 
@@ -16,14 +16,14 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body className={`relative ${inter.className}`}>
-        <Providers>
+
           <Nav />
           <main className="app">{children}</main>
           <div className="main">
             <div className="gradient" />
           </div>
           <FloatingButton />
-        </Providers>
+
       </body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
-const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -19,5 +16,4 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
 };
